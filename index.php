78748dbc51077223df19365c9d23bd66e0d1a017
faiqalizade <faiq.alizade.89@mail.ru>
2019-12-08 03:34:26 +0400

    Strat

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+require 'App/web.php';
+
+?>

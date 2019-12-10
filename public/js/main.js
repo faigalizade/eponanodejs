388c1fd28b037f9cf6765b8eb62ab308de7c79f8
faiqalizade <faiq.alizade.89@mail.ru>
2019-12-10 21:18:47 +0400

    fontawesome and sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 $('#menu').click(function () {
     $('.sidebarBackground').fadeIn(500);
     $('.sidebarContent').css('margin-left','0');
+    $('body').css('overflow','hidden');
 });
 $('.sidebarBackground').click(function(){
     $('.sidebarBackground').fadeOut(500);
-    $('.sidebarContent').css('margin-left','calc(-70% - 2px)');
+    $('.sidebarContent').css('margin-left','calc(-60% - 2px)');
+    $('body').css('overflow','scroll');
 })
